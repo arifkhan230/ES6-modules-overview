9cb1b79fbb82f6659b726dd9b3b075190143b02e
arifkhan101 <arifkhan230122@gmail.com>
2023-12-29 14:33:54 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import Watch from './components/Watch/Watch'
+
+function App() {
+
+  const [watches, setWatches] = useState([]);
+
+  useEffect(()=>{
+    fetch('watches.json')
+    .then(res => res.json())
+    .then(data=> setWatches(data))
+    
+  },[])
+
+  // const watches = [
+  //   {
+  //     id: 1,
+  //     name: "Apple Watch Series 7",
+  //     price: 399,
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "Samsung Galaxy Watch 4",
+  //     price: 249.99,
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "Fitbit Versa 3",
+  //     price: 229.95,
+  //   },
+  //   {
+  //     id: 4,
+  //     name: "Garmin Forerunner 945",
+  //     price: 599.99,
+  //   },
+  //   {
+  //     id: 5,
+  //     name: "Fossil Gen 5",
+  //     price: 249,
+  //   },
+  // ];
+  
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      {
+        watches.map(watch => <Watch key={watch.id} watch={watch}></Watch>)
+      }
+    </>
+  )
+}
+
+export default App
---
+++ src/Utils/calculate.js
@@ -0,0 +1,18 @@
+const add = (first,second) => {
+    return first,second;
+}
+
+const multiply = (first,second) =>{
+    return first * second;
+}
+
+const divideFirstNumberBySecondNumber = (first,second) => {
+    return first / second;
+
+}
+
+export {add, multiply, divideFirstNumberBySecondNumber}
+
+// export default add;
+// const calculator = {add: add}
+// export default calculator;
